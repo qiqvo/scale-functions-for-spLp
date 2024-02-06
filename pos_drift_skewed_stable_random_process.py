796a1d9b7d2d9f1b763fe097ff7dabd10da8fc30
Ben Povar <ben.povar@warwick.ac.uk>
2024-02-06 12:22:56 +0000

    Positively drifting Stable process.

---
+++ pos_drift_skewed_stable_random_process.py
@@ -0,0 +1,27 @@
+from typing import Callable
+import numpy as np
+
+from skewed_stable_random_process import TotallySkewedStableRandomProcess
+
+class PosDriftTotallySkewedStableRandomProcess(TotallySkewedStableRandomProcess):
+    def __init__(self, alpha: float, drift: float) -> None:
+        super().__init__(alpha)
+        self.drift = drift
+
+    def characteristic_function(self, t: np.complex64, time: float, z: np.float64) -> np.complex64:
+        return super().characteristic_function(t, time, z) * np.exp(1j * t * self.drift * time)
+
+    def laplace_transform(self, t: np.float64, time: float, z: np.float64) -> np.float64:
+        return super().laplace_transform(t, time, z) * np.exp(-t * self.drift * time)
+
+    def mean(self, t: np.float64, time: float, z: np.float64) -> np.float64:
+        return super().mean(t, time, z) + self.drift * time
+
+    def variance(self, t: np.float64, time: float, z: np.float64) -> np.float64:
+        return super().variance(t, time, z)
+
+    def sample(self, N: int, time: float, z: np.float64) -> np.ndarray[float]:
+        return super().sample(N, time, z) + self.drift * time
+    
+    def sample_function(self, N: int, theta: Callable, time: float, z: np.float64) -> np.ndarray[float]:
+        return theta(self.sample(N, time, z))
