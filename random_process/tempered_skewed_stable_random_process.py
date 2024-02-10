503f1dbff97211b10983df5af36d68eb327932e2
Ben Povar <ben.povar@warwick.ac.uk>
2024-02-10 13:32:29 +0000

    Straightened the line of inheritance.

--- random_process/tempered_skewed_stable_random_process.py
+++ random_process/tempered_skewed_stable_random_process.py
@@ -1,20 +1,19 @@
 from typing import Callable
 import numpy as np
 
-from random_process.spectrally_negative_levy_random_process import SpectrallyNegativeLevyRandomProcess
+from random_process.tempered_spectrally_negative_levy_random_process import TemperedSpectrallyNegativeLevyRandomProcess
 from random_variable.tempered_stable_random_variable import TemperedTotallySkewedStableRandomVariable
 
-class TemperedTotallySkewedStableRandomProcess(SpectrallyNegativeLevyRandomProcess):
+class TemperedTotallySkewedStableRandomProcess(TemperedSpectrallyNegativeLevyRandomProcess):
     def __init__(self, c: float, alpha: float) -> None:
         self.alpha = alpha
-        self.c = c
         xi = self.get_underlying_xi_for_time(1)
-        super().__init__(xi.mu, xi.sigma, xi.nu, 
+        super().__init__(c, xi.mu, xi.sigma, xi.nu, 
                          xi.nu_unwarranted, xi._max_jump_cutoff)
 
     def get_underlying_xi_for_time(self, time: float) -> TemperedTotallySkewedStableRandomVariable:
         return TemperedTotallySkewedStableRandomVariable(self.c * time**(1/self.alpha), 
                                                          self.alpha, 1, time**(1/self.alpha),
-                                                            # min_jump_cutoff=2**(-4),
-                                                            max_jump_cutoff=2**3)
+                                                         min_jump_cutoff=2**(-8),
+                                                         max_jump_cutoff=2**5)
     
