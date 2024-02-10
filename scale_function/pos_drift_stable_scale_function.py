3f4b3bc7b8c6da118c13c794486766706a1b1c4d
Ben Povar <ben.povar@warwick.ac.uk>
2024-02-10 11:17:45 +0000

    Updated the analytical formulas to drift=0 and q=0 cases.

--- scale_function/pos_drift_stable_scale_function.py
+++ scale_function/pos_drift_stable_scale_function.py
@@ -1,4 +1,5 @@
 import numfracpy
+import scipy
 
 from random_process.pos_drift_skewed_stable_random_process import PosDriftTotallySkewedStableRandomProcess
 from scale_function.scale_function import AnalyticalScaleFunction
@@ -6,13 +7,16 @@ from scale_function.scale_function import AnalyticalScaleFunction
 
 class PosDriftTotallySkewedStableScaleFunction(AnalyticalScaleFunction):
     def __init__(self, q: float, process: PosDriftTotallySkewedStableRandomProcess) -> None:
-        assert process.drift > 0
+        assert process.drift >= 0
         assert q == 0
         super().__init__(q, process)
     
     def value(self, x: float) -> float:
         c = self.process.drift 
         a = self.process.alpha - 1
+        if c == 0:
+            return x ** (a) / scipy.special.gamma(a + 1)
+        
         E = numfracpy.Mittag_Leffler_one(-c * x**(a), a)
         return (1 - E) / c
     
--- scale_function/skewed_stable_scale_function.py
+++ scale_function/skewed_stable_scale_function.py
@@ -1,4 +1,5 @@
 import numfracpy
+import scipy
 
 from scale_function.scale_function import AnalyticalScaleFunction
 from random_process.skewed_stable_random_process import TotallySkewedStableRandomProcess
@@ -10,8 +11,11 @@ class TotallySkewedStableScaleFunction(AnalyticalScaleFunction):
 
     def value(self, x: float) -> float:
         a = self.process.alpha
+        if self.q == 0:
+            return x ** (a - 1) / scipy.special.gamma(a)
+        
         delta = 0.0001
-        dE = numfracpy.Mittag_Leffler_one(self.q * x**(a)+delta, a) - numfracpy.Mittag_Leffler_one(self.q * x**(a), a)
+        dE = numfracpy.Mittag_Leffler_one(self.q * x**(a) + delta, a) - numfracpy.Mittag_Leffler_one(self.q * x**(a), a)
         dE /= delta
         return a * x**(a - 1) * dE
     
