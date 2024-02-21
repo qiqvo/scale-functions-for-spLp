30b3664af7a415ad4372c834ef0e3cb40b0b1bc9
Ben Povar <ben.povar@warwick.ac.uk>
2024-02-21 13:28:13 +0000

    BM scale function.

---
+++ scale_function/brownian_motion_scale_function.py
@@ -0,0 +1,23 @@
+from typing import Union
+import numfracpy
+import numpy as np
+import scipy
+
+from random_process.brownian_motion_random_process import DriftBrownianMotionRandomProcess
+from scale_function.scale_function import AnalyticalScaleFunction
+
+
+class BrownianMotionScaleFunction(AnalyticalScaleFunction):
+    def __init__(self, q: float, process: DriftBrownianMotionRandomProcess) -> None:
+        super().__init__(q, process)
+
+    def value(self, x: float) -> float:
+        x = x / self.process.sigma2
+        if self.q == 0 and self.m == 0:
+            return 2 * x
+
+        a = np.sqrt(2 * self.q * self.process.sigma2 + self.process.drift)
+        res = np.sinh(x * a)
+        res *= np.exp(-self.process.drift * x)
+        res *= 2 / a
+        return res
