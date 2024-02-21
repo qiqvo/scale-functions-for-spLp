ed046bb000ca9e1eb16df45c96beb6c1e7a8723f
Ben Povar <ben.povar@warwick.ac.uk>
2024-02-21 13:28:02 +0000

    Added drift in BM.

--- random_process/brownian_motion_random_process.py
+++ random_process/brownian_motion_random_process.py
@@ -5,28 +5,32 @@ from random_process.random_process import RandomProcess
 from random_variable.normal_random_variable import NormalRandomVariable
 
 
-class BrownianMotionRandomProcess(RandomProcess):
-    def __init__(self) -> None:
-        self.xi = None
+class DriftBrownianMotionRandomProcess(RandomProcess):
+    def __init__(self, drift, sigma2) -> None:
+        super().__init__()
+        self.drift = drift
+        self.sigma2 = sigma2
+        self.xi = self.get_underlying_xi_for_time(1)
 
     def get_underlying_xi_for_time(self, time: float) -> NormalRandomVariable:
-        res = NormalRandomVariable(0, time)
+        res = NormalRandomVariable(time * self.drift, time * self.sigma2)
         return res
     
+    ### the subsequent methods are simplified due to xi being self-similar 
     def characteristic_function(self, t: np.complex64, time: float, z: np.float64) -> np.complex64:
-        return self.get_underlying_xi_for_time(time).characteristic_function(t) * np.exp(1j * z * t)
+        return self.xi.characteristic_function(t * np.power(time, 1/2)) * np.exp(1j * z * t)
 
     def laplace_transform(self, t: np.float64, time: float, z: np.float64) -> np.float64:
-        return self.get_underlying_xi_for_time(time).laplace_transform(t) * np.exp(-z * t)
+        return self.xi.laplace_transform(t * np.power(time, 1/2)) * np.exp(-z * t)
 
     def mean(self, time: float, z: np.float64) -> np.float64:
-        return self.get_underlying_xi_for_time(time).mean() + z
+        return self.xi.mean() * np.power(time, 1/2) + z
 
     def variance(self, time: float, z: np.float64) -> np.float64:
-        return self.get_underlying_xi_for_time(time).variance()
+        return self.xi.variance() * time
 
     def sample(self, N: int, time: float, z: np.float64) -> np.ndarray[float]:
-        return z + self.get_underlying_xi_for_time(time).sample(N)
+        return z + self.xi.sample(N) * np.power(time, 1/2)
     
     def sample_function(self, N: int, theta: Callable, time: float, z: np.float64) -> np.ndarray[float]:
         return theta(self.sample(N, time, z))
